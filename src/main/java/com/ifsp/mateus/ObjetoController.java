98420d9562bcd5748bb3dcdf5c79fbd376137e2f
Haidar. <mateus.gcarvalho@proton.me>
2026-03-18 10:50:16 -0300

    ex 2

--- src/main/java/com/ifsp/mateus/ObjetoController.java
+++ src/main/java/com/ifsp/mateus/ObjetoController.java
@@ -15,4 +15,22 @@ public class ObjetoController {
 
         return "show.html";
     }
+
+    @GetMapping("/table")
+    public String table(Model model) {
+        Product tbk = new Product("To Be Kind", "Vinil do disco To Be Kind, do Swans", 550.00);
+        Product tvo = new Product("The Velvet Underground & Nico", "Vinil do disco The Velvet Underground & Nico", 400.00);
+        Product adc = new Product("A Divina Comédia", "Vinil do disco A Divina Comédia, dos Mutantes", 250.00);
+        Product cd = new Product("Cabeça Dinossauro", "Vinil do disco Cabeça Dinossauro, dos Titãs", 80.00);
+        Product rs = new Product("Rubber Soul", "Vinil do disco Rubber Soul, dos Beatles", 50.00);
+
+
+        model.addAttribute("tbk", tbk);
+        model.addAttribute("tvo", tvo);
+        model.addAttribute("adc", adc);
+        model.addAttribute("cd", cd);
+        model.addAttribute("rs", rs);
+
+        return "table.html";
+    }
 }
